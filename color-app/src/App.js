a90759906b1bfc957cd77919d0272a171c4b4ab2
sahil-1602 <sahildarji16@gmail.com>
2020-10-12 22:54:37 +0530

    Add color Helper to generate dark and light corresponding colors

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import SeedColors from './SeedColors';
 import Palette from './Palette';
+import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
   render() {
+    console.log(generatePalette(SeedColors[4]));
     return (
       <div>
         <Palette palette={SeedColors[4]}/>
