4f5d0bcfb5e004fe144231f3d3656141c278d3fb
sahil-1602 <sahildarji16@gmail.com>
2020-10-17 08:39:49 +0530

    Moved styles out and added constants file

--- color-app/src/DraggableColorBox.js
+++ color-app/src/DraggableColorBox.js
@@ -2,37 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import { SortableElement } from 'react-sortable-hoc';
 import DeleteIcon from '@material-ui/icons/Delete';
-const styles = {
-    root: {
-        width: "25%",
-        height:"25%",
-        margin: "0 0",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-        "&:hover svg": {
-            color: "white",
-            transform:"scale(1.5)"
-        }
-    },
-    boxContent: {
-        position: "absolute",
-        width: "100%",
-        left: "0px",
-        bottom: "10px",
-        padding: "10px",
-        color: "rgba(0,0,0,0.5)",
-        letterSpacing: "1px",
-        textTransform: "uppercase",
-        fontSize: "12px",
-        display: "flex",
-        justifyContent: "space-between"
-    },
-    deleteIcon: {
-        transition: "0.2s ease-in-out"
-    }
-}
+import styles from './styles/DraggableColorBoxStyles';
 
 const DraggableColorBox = SortableElement((props) => {
     const { classes, handleClick, name, color } = props;
---
+++ color-app/src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,33 @@
+const styles = {
+    root: {
+        width: "25%",
+        height:"25%",
+        margin: "0 0",
+        display: "inline-block",
+        position: "relative",
+        cursor: "pointer",
+        marginBottom: "-3.5px",
+        "&:hover svg": {
+            color: "white",
+            transform:"scale(1.5)"
+        }
+    },
+    boxContent: {
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom: "10px",
+        padding: "10px",
+        color: "rgba(0,0,0,0.5)",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    deleteIcon: {
+        transition: "0.2s ease-in-out"
+    }
+}
+
+export default styles;
