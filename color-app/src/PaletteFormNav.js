80150feec6b6b83b0d938c80c7522977348b763d
sahil-1602 <sahildarji16@gmail.com>
2020-10-17 09:04:40 +0530

    Tweaked styles of form

--- color-app/src/PaletteFormNav.js
+++ color-app/src/PaletteFormNav.js
@@ -6,6 +6,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import Button from "@material-ui/core/Button";
 import { Link } from 'react-router-dom';
 import { withStyles } from "@material-ui/core/styles";
@@ -57,7 +58,7 @@ class PaletteFormNav extends Component {
                         onClick={this.props.handleDrawerOpen}
                         className={classNames(classes.menuButton, open && classes.hide)}
                     >
-                    <MenuIcon />
+                    <AddToPhotosIcon />
                     </IconButton>
                     <Typography variant='h6' color='inherit' noWrap>
                             Create A Palette
