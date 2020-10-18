1d44df7611c6771d6b78c3c6a78141ea6e6f3f5e
sahil-1602 <sahildarji16@gmail.com>
2020-10-18 14:45:44 +0530

    Updates

---
+++ precache-manifest.a294258b6512e02b70a741cf57d6149f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4f9aaa4e797142bade9c39915b71d6",
+    "url": "/react-color-app/index.html"
+  },
+  {
+    "revision": "444bddb28df8363805c4",
+    "url": "/react-color-app/static/css/2.e3c8c3f1.chunk.css"
+  },
+  {
+    "revision": "680e816ba64db49a1725",
+    "url": "/react-color-app/static/css/main.54b74f95.chunk.css"
+  },
+  {
+    "revision": "444bddb28df8363805c4",
+    "url": "/react-color-app/static/js/2.c245a549.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-color-app/static/js/2.c245a549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680e816ba64db49a1725",
+    "url": "/react-color-app/static/js/main.32486caa.chunk.js"
+  },
+  {
+    "revision": "13e01f5f492470fa71cd",
+    "url": "/react-color-app/static/js/runtime-main.bd209fdc.js"
+  },
+  {
+    "revision": "ef1763deb7c6d92f7331b07ac275368c",
+    "url": "/react-color-app/static/media/bg.ef1763de.svg"
+  }
+]);
